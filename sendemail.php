0a2cb4df497fe0f9ae83b0e5b99de9c67493a89e
Ebenezer Gasonoo <iamknightrae@gmail.com>
2017-07-22 16:07:22 +0100

    About us page done
    100 percent

--- sendemail.php
+++ sendemail.php
@@ -3,7 +3,7 @@ $name       = @trim(stripslashes($_POST['name']));
 $from       = @trim(stripslashes($_POST['email'])); 
 $subject    = @trim(stripslashes($_POST['subject'])); 
 $message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'info@gasbrosnetwork.com';
+$to   		= 'info@gasbrosgamingnetwork.com';
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
